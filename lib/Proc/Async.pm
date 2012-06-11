3edf1b7c1c48e28a04042c18b080ec7c44297ad3
Martin Senger <martin.senger@kaust.edu.sa>
2012-06-11 13:43:05 +0300

    started

---
+++ lib/Proc/Async.pm
@@ -0,0 +1,16 @@
+#-----------------------------------------------------------------
+# Proc::Async
+# Author: Martin Senger <martin.senger@gmail.com>
+# For copyright and disclaimer se below.
+#
+# ABSTRACT: Running and monitoring processes asynchronously
+# PODNAME: Proc::Async
+#-----------------------------------------------------------------
+
+use warnings;
+use strict;
+package Proc::Async;
+
+# VERSION
+
+1;
